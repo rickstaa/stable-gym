f6ac9ff968e4ec527eb1d7e4ca357b4e43a33408
rickstaa <rick.staa@outlook.com>
2020-12-03 20:44:08 +0100

    :sparkles: Adds Ex3_EKF environment git submodule
    This commits replaces the Ex3_EKF environment folder with a submodule.

--- simzoo/envs/Ex3_EKF/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-# Check if used as name_space package
-if "simzoo" in sys.modules:
-    from simzoo.envs.Ex3_EKF.Ex3_EKF import Ex3_EKF
-else:
-    from machine_learning_control.simzoo.simzoo.envs.Ex3_EKF.Ex3_EKF import Ex3_EKF
