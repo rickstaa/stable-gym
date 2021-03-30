bd60295edd846d7d0d30108ed459f8d26bc7ff66
Rick Staa <rick.staa@outlook.com>
2021-03-30 12:31:20 +0200

    :bug: Fixes CartPoleCost registration bug

--- simzoo/envs/biological/oscillator/__init__.py
+++ simzoo/envs/biological/oscillator/__init__.py
@@ -4,15 +4,16 @@ import sys
 
 # Import simzoo stand-alone package or name_space package (blc)
 if "simzoo" in sys.modules:
-    from simzoo.envs.oscillator.oscillator import Oscillator
+    from simzoo.envs.biological.oscillator.oscillator import Oscillator
 elif importlib.util.find_spec("simzoo") is not None:
     Oscillator = getattr(
-        importlib.import_module("simzoo.envs.oscillator.oscillator"), "Oscillator"
+        importlib.import_module("simzoo.envs.biological.oscillator.oscillator"),
+        "Oscillator",
     )
 else:
     Oscillator = getattr(
         importlib.import_module(
-            "bayesian_learning_control.simzoo.simzoo.envs.oscillator.oscillator"
+            "bayesian_learning_control.simzoo.simzoo.envs.biological.oscillator.oscillator"
         ),
         "Oscillator",
     )
--- simzoo/envs/classic_control/cart_pole_cost/__init__.py
+++ simzoo/envs/classic_control/cart_pole_cost/__init__.py
@@ -4,15 +4,18 @@ import sys
 
 # Import simzoo stand-alone package or name_space package (blc)
 if "simzoo" in sys.modules:
-    from simzoo.envs.cart_pole.CartPole import CartPoleCost
+    from simzoo.envs.classic_control.cart_pole_cost.cart_pole_cost import CartPoleCost
 elif importlib.util.find_spec("simzoo") is not None:
     CartPole = getattr(
-        importlib.import_module("simzoo.envs.cart_pole.CartPole"), "CartPoleCost"
+        importlib.import_module(
+            "simzoo.envs.classic_control.cart_pole_cost.cart_pole_cost"
+        ),
+        "CartPoleCost",
     )
 else:
     CartPole = getattr(
         importlib.import_module(
-            "bayesian_learning_control.simzoo.simzoo.envs.cart_pole.CartPole"
+            "bayesian_learning_control.simzoo.simzoo.envs.classic_control.cart_pole_cost.cart_pole_cost"
         ),
         "CartPoleCost",
     )
