1a0469d2fbd3def46fc57bbcded07ea39378cfbf
rickstaa <rick.staa@outlook.com>
2023-06-05 21:59:37 +0200

    fix: fix incorrect stable_gym import

--- stable_gym/envs/classic_control/cartpole_cost/__init__.py
+++ stable_gym/envs/classic_control/cartpole_cost/__init__.py
@@ -1,7 +1,7 @@
 """A synthetic oscillatory network of transcriptional regulators gymnasium environment.
 """
 # fmt: off
-from stabe_gym.envs.classic_control.cartpole_cost.cartpole_cost import \
+from stable_gym.envs.classic_control.cartpole_cost.cartpole_cost import \
     CartPoleCost  # noqa: F401
 
 # fmt: off
