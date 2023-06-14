4d1c8a2a3344505d6358d577484346cf943d1c6e
rickstaa <rick.staa@outlook.com>
2023-06-14 10:03:39 +0200

    ci: add release please
    Adds release-please action since standard-version has been depricated.

--- stable_gym/__init__.py
+++ stable_gym/__init__.py
@@ -4,8 +4,8 @@ import gymnasium as gym
 from gymnasium.envs.registration import register
 
 # Make module version available.
-from ._version import version as __version__  # noqa: F401
-from ._version import version_tuple as __version_tuple__  # noqa: F401
+from stable_gym.version import version as __version__  # noqa: F401
+from stable_gym.version import version_tuple as __version_tuple__  # noqa: F401
 
 # Available environments
 ENVS = {
