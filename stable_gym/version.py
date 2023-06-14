5e78d629c99c6981d7b46be02517bdc1c384c888
rickstaa <rick.staa@outlook.com>
2023-06-14 13:46:24 +0200

    refactor: change version parameter

--- stable_gym/version.py
+++ stable_gym/version.py
@@ -1,3 +1,3 @@
 # Stores the package version number so that it can be accessed from other modules.
-version = "0.6.36"
-version_tuple = version.split(".")
+__version__ = "0.6.36"
+__version_tuple__ = __version__.split(".")
