28f7875aa244534b1f60873d04fa1e6b00ab4e8f
rickstaa <rick.staa@outlook.com>
2023-07-11 21:37:15 +0200

    feat: add mujoco envs health penalty
    This commit adds a health penalty to certain Mujoco environments. This penalty is optional and can
    be enabled using the 'include_health_penalty' environment argument.

--- stable_gym/envs/mujoco/hopper_cost/hopper_cost.py
+++ stable_gym/envs/mujoco/hopper_cost/hopper_cost.py
@@ -3,6 +3,7 @@
 import gymnasium as gym
 import matplotlib.pyplot as plt
 import numpy as np
+from gymnasium import utils
 from gymnasium.envs.mujoco.hopper_v4 import HopperEnv
 
 import stable_gym  # NOTE: Required to register environments. # noqa: F401
@@ -11,7 +12,7 @@ RANDOM_STEP = True  # Use random action in __main__. Zero action otherwise.
 
 
 # TODO: Update solving criteria after training.
-class HopperCost(HopperEnv):
+class HopperCost(HopperEnv, utils.EzPickle):
     """Custom Hopper gymnasium environment.
 
     .. note::
@@ -26,7 +27,8 @@ class HopperCost(HopperEnv):
 
         -   The objective was changed to a velocity-tracking task. To do this, the reward
             is replaced with a cost. This cost is the squared difference between the
-            Hopper's forward velocity and a reference value (error).
+            Hopper's forward velocity and a reference value (error). Additionally, also
+            a control cost and health penalty can be included in the cost.
 
         The rest of the environment is the same as the original Hopper environment.
         Below, the modified cost is described. For more information about the environment
@@ -36,7 +38,7 @@ class HopperCost(HopperEnv):
     Modified cost:
         .. math::
 
-            cost = w_{forward} \\times (x_{velocity} - x_{reference\_x\_velocity})^2 + w_{ctrl} \\times c_{ctrl}
+            cost = w_{forward} \\times (x_{velocity} - x_{reference\_x\_velocity})^2 + w_{ctrl} \\times c_{ctrl} + p_{health}
 
     Solved Requirements:
         Considered solved when the average cost is less than or equal to 50 over
@@ -50,19 +52,27 @@ class HopperCost(HopperEnv):
             env = gym.make("HopperCost-v1")
 
     Attributes:
-        reference_forward_velocity (float): The forward velocity that the agent should try
-            to track.
-        include_ctrl_cost (bool): Whether you also want to penalize the Hopper if it
-            takes actions that are too large.
-        forward_velocity_weight (float): The weight used to scale the forward velocity error.
+        state (numpy.ndarray): The current system state.
+        t (float): The current time step.
+        dt (float): The environment step size.
+        reference_forward_velocity (float): The forward velocity that the agent should
+            try to track.
     """  # noqa: E501, W605
 
     def __init__(
         self,
         reference_forward_velocity=1.0,
-        include_ctrl_cost=True,
         forward_velocity_weight=1.0,
-        ctrl_cost_weight=None,
+        include_ctrl_cost=False,
+        include_health_penalty=True,
+        health_penalty_size=10,
+        ctrl_cost_weight=1e-3,
+        terminate_when_unhealthy=True,
+        healthy_state_range=(-100.0, 100.0),
+        healthy_z_range=(0.7, float("inf")),
+        healthy_angle_range=(-0.2, 0.2),
+        reset_noise_scale=5e-3,
+        exclude_current_positions_from_observation=True,
         **kwargs,
     ):
         """Constructs all the necessary attributes for the HopperCost instance.
@@ -70,23 +80,104 @@ class HopperCost(HopperEnv):
         Args:
             reference_forward_velocity (float, optional): The forward velocity that the
                 agent should try to track. Defaults to ``1.0``.
-            include_ctrl_cost (bool, optional): Whether you also want to penalize the
-                Hopper if it takes actions that are too large. Defaults to ``True``.
             forward_velocity_weight (float, optional): The weight used to scale the
                 forward velocity error. Defaults to ``1.0``.
-            ctrl_cost_weight (_type_, optional): The weight used to scale the control
-                cost. Defaults to ``None`` meaning that the default value of the
-                :attr:`~gymnasium.envs.mujoco.hopper_v4.HopperEnv.ctrl_cost_weight`
-                attribute is used.
-        """  # noqa: E501
-        super().__init__(**kwargs)
+            include_ctrl_cost (bool, optional): Whether you also want to penalize the
+                hopper if it takes actions that are too large. Defaults to ``False``.
+            include_health_penalty (bool, optional): Whether to penalize the hopper if
+                it becomes unhealthy (i.e. if it falls over). Defaults to ``True``.
+            health_penalty_size (int, optional): The size of the unhealthy penalty.
+                Defaults to ``10``.
+            ctrl_cost_weight (float, optional): The weight used to scale the control
+                cost. Defaults to ``1e-3``.
+            terminate_when_unhealthy (bool, optional): Whether to terminate the episode
+                when the hopper becomes unhealthy. Defaults to ``True``.
+            healthy_state_range (tuple, optional): The range of healthy states. Defaults
+                to ``(-100.0, 100.0)``.
+            healthy_z_range (tuple, optional): The range of healthy z values. Defaults
+                to ``(0.7, float("inf"))``.
+            healthy_angle_range (tuple, optional): The range of healthy angles. Defaults
+                to ``(-0.2, 0.2)``.
+            reset_noise_scale (float, optional): Scale of random perturbations of the
+                initial position and velocity. Defaults to ``5e-3``.
+            exclude_current_positions_from_observation (bool, optional): Whether to omit
+                the x- and y-coordinates of the front tip from observations. Excluding
+                the position can serve as an inductive bias to induce position-agnostic
+                behaviour in policies. Defaults to ``True``.
+        """
         self.reference_forward_velocity = reference_forward_velocity
-        self.include_ctrl_cost = include_ctrl_cost
-        self._ctrl_cost_weight = (
-            ctrl_cost_weight if ctrl_cost_weight else self._ctrl_cost_weight
-        )
-        self.forward_velocity_weight = forward_velocity_weight
+        self._forward_velocity_weight = forward_velocity_weight
+        self._include_ctrl_cost = include_ctrl_cost
+        self._include_health_penalty = include_health_penalty
+        self._health_penalty_size = health_penalty_size
+
         self.state = None
+        self.t = 0.0
+
+        # Initialize the HopperEnv class.
+        super().__init__(
+            ctrl_cost_weight=ctrl_cost_weight,
+            terminate_when_unhealthy=terminate_when_unhealthy,
+            healthy_state_range=healthy_state_range,
+            healthy_z_range=healthy_z_range,
+            healthy_angle_range=healthy_angle_range,
+            reset_noise_scale=reset_noise_scale,
+            exclude_current_positions_from_observation=exclude_current_positions_from_observation,  # noqa: E501
+            **kwargs,
+        )
+
+        # Reinitialize the EzPickle class.
+        # NOTE: Done to ensure the args of the HopperCost class are also pickled.
+        # NOTE: Ensure that all args are passed to the EzPickle class!
+        utils.EzPickle.__init__(
+            self,
+            reference_forward_velocity,
+            forward_velocity_weight,
+            include_ctrl_cost,
+            include_health_penalty,
+            health_penalty_size,
+            ctrl_cost_weight,
+            terminate_when_unhealthy,
+            healthy_state_range,
+            healthy_z_range,
+            healthy_angle_range,
+            reset_noise_scale,
+            exclude_current_positions_from_observation,
+            **kwargs,
+        )
+
+    def cost(self, x_velocity, ctrl_cost):
+        """Compute the cost of a given x velocity and control cost.
+
+        Args:
+            x_velocity (float): The Hopper's x velocity.
+            ctrl_cost (float): The control cost.
+
+        Returns:
+            (tuple): tuple containing:
+
+                - cost (float): The cost of the action.
+                - info (:obj:`dict`): Additional information about the cost.
+        """
+        velocity_cost = self._forward_velocity_weight * np.square(
+            x_velocity - self.reference_forward_velocity
+        )
+        cost = velocity_cost
+        if self._include_ctrl_cost:
+            cost += ctrl_cost
+
+        # Add extra penalty if the hopper becomes unhealthy.
+        health_penalty = 0.0
+        if self._include_health_penalty:
+            if not self.is_healthy:
+                health_penalty = self._health_penalty_size
+                cost += health_penalty
+
+        return cost, {
+            "cost_velocity": velocity_cost,
+            "cost_ctrl": ctrl_cost,
+            "penalty_health": health_penalty,
+        }
 
     def step(self, action):
         """Take step into the environment.
@@ -111,49 +202,50 @@ class HopperCost(HopperEnv):
                 - info (:obj`dict`): Additional information about the environment.
         """
         obs, _, terminated, truncated, info = super().step(action)
+
         self.state = obs
+        self.t = self.t + self.dt
 
         ctrl_cost = super().control_cost(action)
         cost, cost_info = self.cost(info["x_velocity"], ctrl_cost)
 
         # Update info.
-        info["reward_fwd"] = cost_info["reward_fwd"]
-        info["forward_reward"] = cost_info["reward_fwd"]
-        info["reward_ctrl"] = -ctrl_cost
+        info["cost_velocity"] = cost_info["cost_velocity"]
         info["cost_ctrl"] = cost_info["cost_ctrl"]
+        info["penalty_health"] = cost_info["penalty_health"]
 
         return obs, cost, terminated, truncated, info
 
-    def cost(self, x_velocity, ctrl_cost):
-        """Compute the cost of the action.
+    def reset(self, seed=None, options=None):
+        """Reset gymnasium environment.
 
         Args:
-            x_velocity (float): The Hopper's x velocity.
-            ctrl_cost (float): The control cost.
+            seed (int, optional): A random seed for the environment. By default
+                ``None``.
+            options (dict, optional): A dictionary containing additional options for
+                resetting the environment. By default ``None``. Not used in this
+                environment.
 
         Returns:
             (tuple): tuple containing:
 
-                - cost (float): The cost of the action.
-                - info (:obj:`dict`): Additional information about the cost.
+                - observation (:obj:`numpy.ndarray`): Array containing the current
+                  observation.
+                - info (:obj:`dict`): Dictionary containing additional information.
         """
-        reward_fwd = self.forward_velocity_weight * np.square(
-            x_velocity - self.reference_forward_velocity
-        )
-        cost = reward_fwd
-        if self.include_ctrl_cost:
-            cost += ctrl_cost
-        return cost, {"reward_fwd": reward_fwd, "cost_ctrl": ctrl_cost}
+        observation, info = super().reset(seed=seed, options=options)
+
+        self.state = observation
+        self.t = 0.0
+
+        return observation, info
 
     @property
-    def ctrl_cost_weight(self):
-        """Property that returns the control cost weight."""
-        return self._ctrl_cost_weight
-
-    @ctrl_cost_weight.setter
-    def ctrl_cost_weight(self, value):
-        """Setter for the control cost weight."""
-        self._ctrl_cost_weight = value
+    def tau(self):
+        """Alias for the environment step size. Done for compatibility with the
+        other gymnasium environments.
+        """
+        return self.dt
 
 
 if __name__ == "__main__":
