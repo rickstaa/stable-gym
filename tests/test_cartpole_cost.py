5b27c3a1f60ee66173dc9cd969e89cf87512df1c
Rick Staa <rick.staa@outlook.com>
2023-07-23 11:43:20 +0200

    test: improve tests with fixtures (#247)

--- tests/test_cartpole_cost.py
+++ tests/test_cartpole_cost.py
@@ -8,63 +8,79 @@ import gymnasium as gym
 import numpy as np
 from gymnasium.logger import ERROR
 
+import pytest
 import stable_gym  # noqa: F401
 
 gym.logger.set_level(ERROR)
 
 
 class TestCartPoleCostEqual:
-    # Make original CartPole environment.
-    env = gym.make("CartPole")
-    # Make CartPoleCost environment.
-    env_cost = gym.make("CartPoleCost")
+    @pytest.fixture
+    def env_original(self):
+        """Create original CartPole environment."""
+        return gym.make("CartPole")
 
-    # Overwrite changed parameters.
-    env_cost.unwrapped.force_mag = 10.0
-    env_cost.unwrapped.theta_threshold_radians = 12 * 2 * math.pi / 360
-    env_cost.unwrapped.x_threshold = 2.4
-    env_cost.unwrapped.max_v = np.finfo(np.float64).max
-    env_cost.unwrapped.max_x = np.finfo(np.float64).max
-    env_cost.unwrapped._init_state_range = {
-        "low": [-0.05, -0.05, -0.05, -0.05],
-        "high": [0.05, 0.05, 0.05, 0.05],
-    }
+    @pytest.fixture
+    def env_cost_aligned(self):
+        """Create CartPoleCost environment and align the parameters with the
+        original CartPole environment.
+        """
+        env_cost = gym.make(
+            "CartPoleCost",
+        )
 
-    def test_equal_reset(self):
+        # Overwrite changed parameters.
+        env_cost.unwrapped.force_mag = 10.0
+        env_cost.unwrapped.theta_threshold_radians = 12 * 2 * math.pi / 360
+        env_cost.unwrapped.x_threshold = 2.4
+        env_cost.unwrapped.max_v = np.finfo(np.float64).max
+        env_cost.unwrapped.max_x = np.finfo(np.float64).max
+        env_cost.unwrapped._init_state_range = {
+            "low": [-0.05, -0.05, -0.05, -0.05],
+            "high": [0.05, 0.05, 0.05, 0.05],
+        }
+        return env_cost
+
+    @pytest.fixture
+    def env_cost(self):
+        """Create CartPoleCost environment."""
+        return gym.make(
+            "CartPoleCost",
+        )
+
+    def test_equal_reset(self, env_original, env_cost_aligned):
         """Test if reset behaves the same."""
         # Perform reset and check if observations are equal.
-        observation, _ = self.env.reset(seed=42)
-        observation_cost, _ = self.env_cost.reset(seed=42)
+        observation, _ = env_original.reset(seed=42)
+        observation_cost, _ = env_cost_aligned.reset(seed=42)
         assert np.allclose(
             observation, observation_cost
         ), f"{observation} != {observation_cost}"
 
-    def test_equal_steps(self):
+    def test_equal_steps(self, env_original, env_cost_aligned):
         """Test if steps behave the same."""
         # Perform several steps and check if observations are equal.
-        self.env.reset(seed=42), self.env_cost.reset(seed=42)
+        env_original.reset(seed=42), env_cost_aligned.reset(seed=42)
         for _ in range(10):
             discrete_action = random.randint(0, 1)
             continuous_action = (
                 np.array([-10]) if discrete_action == 0 else np.array([10])
             )
-            observation, _, _, _, _ = self.env.step(discrete_action)
-            observation_cost, _, _, _, _ = self.env_cost.step(continuous_action)
+            observation, _, _, _, _ = env_original.step(discrete_action)
+            observation_cost, _, _, _, _ = env_cost_aligned.step(continuous_action)
             assert np.allclose(
                 observation, observation_cost
             ), f"{observation} != {observation_cost}"
 
-    def test_snapshot(self, snapshot):
+    def test_snapshot(self, snapshot, env_cost):
         """Test if the 'CartPoleCost' environment is still equal to snapshot."""
-        observation, info = self.env_cost.reset(seed=42)
+        observation, info = env_cost.reset(seed=42)
         assert (observation == snapshot).all()
         assert info == snapshot
-        self.env_cost.action_space.seed(42)
+        env_cost.action_space.seed(42)
         for _ in range(5):
-            action = self.env_cost.action_space.sample()
-            observation, reward, terminated, truncated, info = self.env_cost.step(
-                action
-            )
+            action = env_cost.action_space.sample()
+            observation, reward, terminated, truncated, info = env_cost.step(action)
             assert (observation == snapshot).all()
             assert reward == snapshot
             assert terminated == snapshot
--- tests/test_oscillator.py
+++ tests/test_oscillator.py
@@ -4,27 +4,32 @@ environment.
 import gymnasium as gym
 from gymnasium.logger import ERROR
 
+import pytest
 import stable_gym  # noqa: F401
 
 gym.logger.set_level(ERROR)
 
 
 class TestOscillator:
-    env = gym.make("Oscillator", exclude_reference_error_from_observation=False)
+    @pytest.fixture
+    def env(self):
+        """Create Oscillator environment."""
+        return gym.make("Oscillator", exclude_reference_error_from_observation=False)
 
-    def test_reset(self, snapshot):
+    def test_reset(self, snapshot, env):
         """Test if reset is still equal to the last snapshot."""
-        observation, info = self.env.reset(seed=42)
+        observation, info = env.reset(seed=42)
         assert (observation == snapshot).all()
         assert info == snapshot
 
-    def test_step(self, snapshot):
+    def test_step(self, snapshot, env):
         """Test if steps behave the same."""
         # Perform steps and check if observations are equal.
-        self.env.action_space.seed(42)
+        _, _ = env.reset(seed=42)
+        env.action_space.seed(42)
         for _ in range(5):
-            action = self.env.action_space.sample()
-            observation, reward, terminated, truncated, info = self.env.step(action)
+            action = env.action_space.sample()
+            observation, reward, terminated, truncated, info = env.step(action)
             assert (observation == snapshot).all()
             assert reward == snapshot
             assert terminated == snapshot
