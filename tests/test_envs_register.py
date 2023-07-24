51df02eb0f053dea579a1c0efeccc8e1e86828f9
Rick Staa <rick.staa@outlook.com>
2023-07-24 21:25:49 +0200

    test: fix PyFlyt runtime error (#254)
    This commit handles the RuntimeError error that is thrown by PyFlyt when
    PyBullet is not build with numpy support. See
    https://github.comhoots/PyFlyt/pull/8 for more information.

--- tests/test_envs_register.py
+++ tests/test_envs_register.py
@@ -3,11 +3,23 @@ registered.
 """
 import pytest
 from gymnasium import envs
+import pybullet
 
 from stable_gym import ENVS
+
+QUADX_ENVS = [env_name for env_name in ENVS.keys() if "Quad" in env_name]
 
 
 @pytest.mark.parametrize("env_name", ENVS.keys())
 def test_env_reg(env_name):
+    # TODO: Can be removed if https://github.com/jjshoots/PyFlyt/issues/1 is resolved.
+    # Skip if QuadX environment and pybullet has no numpy support.
+    if env_name in QUADX_ENVS:
+        if not pybullet.isNumpyEnabled():
+            pytest.skip(
+                "pybullet was not built with numpy support. Please rebuild pybullet "
+                "with numpy enabled."
+            )
+
     env = envs.make(env_name)
     assert env.spec.id == env_name
